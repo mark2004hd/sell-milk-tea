201c781876061be9127a42553dbeacde2b99b8d1
Mark2004.Dev <vanson2004tkhd@gmail.com>
2025-04-15 20:38:50 +0700

    update

--- env.d.ts
+++ env.d.ts
@@ -1,4 +1,5 @@
 declare module '@env' {
     export const VITE_SUPABASE_URL: string;
-    export const VITE_SUPABASE_ANON_KEY: string;
+  export const VITE_SUPABASE_ANON_KEY: string;
+  export const LOCAL_IPV4_ADDRESS: string;
   }
